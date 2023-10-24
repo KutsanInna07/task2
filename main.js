5920d0bc0e2ec7476e92b878fbcbb085fce6a5f9
Inna018 <122346373+Inna018@users.noreply.github.com>
2023-10-24 09:30:40 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+function fizzBuzz() {
+  const n = parseInt(document.getElementById("number").value);
+  let answer = "";
+
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      answer = "FizzBuzz";
+    } else if (i % 3 === 0) {
+      answer = "Fizz";
+    } else if (i % 5 === 0) {
+      answer = "Buzz";
+    } else {
+      answer = i.toString();
+    }
+  }
+
+  document.getElementById("result").innerHTML = answer;
+}
